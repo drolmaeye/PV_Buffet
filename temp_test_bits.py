56b9d8c6a8dc24e0a4e4b6361c50241db1c47960
Jesse Smith <jssmith@ciw.edu>
2017-01-20 14:02:51 -0600

    config_record is repaired

--- temp_test_bits.py
+++ temp_test_bits.py
@@ -1,3 +1,21 @@
 from Tkinter import *
 from tktable import *
+from epics import *
+import collections
 
+d = collections.OrderedDict()
+
+cenxmdesc = PV('XPSGP:m1.DESC')
+cenxmrbv = PV('XPSGP:m1.RBV')
+cenymdesc = PV('XPSGP:m2.DESC')
+cenymrbv = PV('XPSGP:m2.RBV')
+
+print cenxmdesc.value
+print cenxmrbv.value
+print cenymdesc.value
+print cenymrbv.value
+
+d[cenxmdesc.value] = cenxmrbv.value
+d[cenymdesc.value] = cenymrbv.value
+
+print d.values()
